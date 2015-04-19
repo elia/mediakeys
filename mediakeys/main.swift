4218b6f6e4dfa3a0f1f8db65ff1e6b11f26b358c
Elia Schito <elia@schito.me>
2015-04-19 22:58:08 +0200

    Initial Commit

---
+++ mediakeys/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  mediakeys
+//
+//  Created by Elia on 19/04/15.
+//  Copyright (c) 2015 Elia Schito. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
